5468f38b0dfbf2049f219e611ec144003d4f9ce2
Abdurauf7 <Abdurauf98@mail.ru>
2024-06-24 09:41:36 +0500

    cors

--- server/src/utils/helpers.ts
+++ server/src/utils/helpers.ts
@@ -44,11 +44,12 @@ export const errorsHandler = (status: number, message?: string | null) => {
 
 export const corsHandler:RequestHandler = (req,res,next) =>{
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // res.setHeader('Access-Control-Allow-Credentials','true')
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, PATCH, DELETE'
   );
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 }
 
